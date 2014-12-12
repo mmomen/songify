905235ffe3bcf72317962626aa88e2300e4518de
Mazdak Momen <mazdak.momen@gmail.com>
2014-12-11 20:41:26 -0800

    Added album view to disable just added album, updated index view to see list of all current albums, songify.rb contains album entity and repo which works with pg now, server.rb updated to reflect changes and added logic to handle duplicate album title using duplicate_album view.

--- server.rb
+++ server.rb
@@ -6,10 +6,25 @@ require_relative 'songify.rb'
 
 set :bind, '0.0.0.0' # This is needed for Vagrant
 
-
 get '/' do
-  @message = "Hello World"
-  @remarks = ["I am", "A Group Of", "Messages", "Make Sure", "You Check", "The ERB tags"]
+  @albums = Songify::AlbumRepo.get_albums
   erb :index
 end
 
+post '/album' do
+  @album = Songify::AlbumRepo.add_album(params["album-title"])
+  if @album.nil?
+    erb :duplicate_album
+  else
+    erb :album
+  end
+
+  # params.to_s
+  # "Your album: " + params["album-title"]
+end
+
+get '/album/*' do
+  
+end
+
+# {"album-title"=>"asd", "track-1"=>"asd", "track-2"=>"", "track-3"=>"", "track-4"=>"", "track-5"=>"", "track-6"=>"", "track-7"=>"", "track-8"=>"", "track-9"=>"", "track-10"=>""}
--- songify.rb
+++ songify.rb
@@ -1,4 +1,63 @@
+require 'pg'
+
 module Songify
+  class Album
+    attr_reader :id, :title
+    def initialize(id, title)
+      @id = id
+      @title = title
+    end
+  end
+  
+  class AlbumRepo
+    @@db = PG.connect(dbname: 'songify-db')
+    
+    def self.create_table
+      command = <<-SQL
+      CREATE TABLE IF NOT EXISTS albums(
+        id SERIAL,
+        title text,
+        PRIMARY KEY( id )
+      );
+      SQL
+
+      @@db.exec(command)
+    end
+
+    def self.get_albums
+      command = <<-SQL
+      SELECT * FROM albums;
+      SQL
+
+      result = @@db.exec(command)
+      result.values.map do |x|
+        Album.new(x[0], x[1])
+      end
+    end
+
+    def self.add_album(title)
+      create_table
+      command = <<-SQL
+      INSERT INTO albums( title )
+      SELECT '#{title}' 
+      WHERE NOT EXISTS (SELECT * FROM albums WHERE title = '#{title}' )
+      RETURNING *;
+      SQL
+
+      result = @@db.exec(command)
+      data = result.values[0]
+      if data.nil?
+        return nil
+      else
+        Songify::Album.new(data[0], data[1])
+      end
+
+    end
+    
+    def self.clear_data 
+      # TODO: implement
+    end
+  end
 end
 
 # require all lib/ entities and repos files here
